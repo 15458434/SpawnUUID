d13e2c0d30a65fff28afc11eceb67b9fbb8d5d07
Mark Cornelisse <info@markcornelisse.nl>
2018-02-16 11:06:38 +0100

    - Handles window reopen when pressing icon in the dock.

--- SpawnUUID/AppDelegate.m
+++ SpawnUUID/AppDelegate.m
@@ -14,10 +14,34 @@
 
 @implementation AppDelegate
 
+- (void)reopenMainWindow {
+    NSApplication *app = [NSApplication sharedApplication];
+    if (app.windows.count == 0) {
+        NSStoryboard *storyBoard = [NSStoryboard storyboardWithName:@"Main" bundle:nil];
+        NSWindowController *windowController = [storyBoard instantiateControllerWithIdentifier:@"MainWindowController"];
+        [windowController showWindow:self];
+    } else {
+        NSWindow *window = app.mainWindow;
+        if (window) {
+            [window makeKeyAndOrderFront:self];
+        } else {
+            NSWindow *window = app.windows.firstObject;
+            [window makeKeyAndOrderFront:self];
+        }
+    }
+}
+
+#pragma mark - NSApplicationDelegate
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
 }
 
+- (BOOL)applicationShouldHandleReopen:(NSApplication *)sender hasVisibleWindows:(BOOL)flag {
+    [self reopenMainWindow];
+    
+    return YES;
+}
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
